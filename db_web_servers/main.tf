b4acb538e9ff6943a32120040fb892f593e0bd5f
zacharyrgonzales <zacharyrgonzales@gmail.com>
2020-05-07 19:07:55 -0500

    new tf script

--- db_web_servers/main.tf
+++ db_web_servers/main.tf
@@ -1,51 +1,75 @@
-locals {
-    db_instance_class = "db.t2.micro"
-    ec2_ami = "ami-03e33c1cefd1d3d74"
-    ec2_instance_type = "t2.micro"
-}
-
-resource "aws_db_instance" "RDS_Instance" {
-    allocated_storage = 20
-    engine = "mysql"
-    instance_class = "${local.db_instance_class}"
-    password = "db1"
-    username = "terraform"
-}
-
-resource "aws_instance" "EC2_instance" {
-    ami = "${local.ec2_ami}"
-    instance_type = "${local.ec2_instance_type}"
-    security_groups = ["${aws_security_group.web_traffic1.name}"]
-    
-    provisioner "file" {
-        source = "script.sh"
-        destination = "/user/zach/documents/script.sh"
-    }
+#create db server
+resource "aws_instance" "db" {
+    ami = "ami-0323c3dd2da7fb37d"
+    instance_type = "t2.micro"
 
     tags = {
-        public_ip = ""
+      name = "DB"
     }
 }
 
+#create bootstrapped web server
+resource "aws_instance" "web" {
+    ami = "ami-0323c3dd2da7fb37d"
+    instance_type = "t2.micro"
+    user_data = "${file("server-script.sh")}"
+    security_groups = ["${aws_security_group.allow_tls.name}"]
+
+    tags = {
+      name = "Web"
+    }
+}
+#create elastic ip and assign to web server
 resource "aws_eip" "lb" {
-  instance = "${aws_instance.EC2_instance.id}"
+  instance = "${aws_instance.web.id}"
 }
 
+#create security group for web server for public access
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
 
-resource "aws_security_group" "web_traffic1" {
-    name = "web_traffic1"
-    description = "to allow web traffic"
-    ingress = {
-        description = "https traffic rule"
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
 
 output "db_ip" {
-  value = "${aws_instance.EC2_instance.public_ip}"
-      
+  value = "${aws_instance.db.private_ip}"
 }
 
+output "web_ip" {
+  value = "${aws_eip.lb.public_ip}"
+}
